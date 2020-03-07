2539f697ab7162164258a02a0cbad32138845223
Макаров Максим <MisterMaks97@mail.ru>
2020-03-08 02:03:27 +0300

    Добавил описание функции get_other_list()

--- homeworks/homework-1-2/task_2.py
+++ homeworks/homework-1-2/task_2.py
@@ -76,9 +76,7 @@ class CCoolList(list):
         return sum(self.lst) >= sum(self.other.lst)
 
     def get_other_list(self, other):
-        """
-        Получение объекта CCoolList из other, если это возможно, иначе ошибка
-        """
+        """Получение объекта CCoolList из other, если это возможно, иначе ошибка"""
         if type(other) is list:
             self.other = CCoolList(other)
         elif type(other) is CCoolList:
